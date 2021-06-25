6557743d8aa55676f5ea74d0a45ca8de05d2efd5
Rafael Bahiense <rafaelbahiense@id.uff.br>
2021-06-25 20:18:49 -0300

    heroku deploy

--- src/config/database.ts
+++ src/config/database.ts
@@ -11,9 +11,8 @@ const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT } = process.env;
 const { Pool } = pg;
 
 export const connectionDB = new Pool({
-  user: DB_USERNAME,
-  host: DB_HOST,
-  port: DB_PORT as unknown as number,
-  database: DB_DATABASE,
-  password: DB_PASSWORD,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+      rejectUnauthorized: false
+  }
 });
