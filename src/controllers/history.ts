5ed6d94dba045973884efc1fcd25d495bcfff6f9
Rafael Bahiense <rafaelbahiense@id.uff.br>
2021-06-25 11:08:09 -0300

    added total

--- src/controllers/history.ts
+++ src/controllers/history.ts
@@ -1,26 +1,41 @@
 import { Request, Response } from "express";
 import dayjs from "dayjs";
 
-import {connectionDB} from "../config/database";
-import {Token} from "../schemas/schemas";
+import { connectionDB } from "../config/database";
+import { Token } from "../schemas/schemas";
 import errorHandler from "./errorHandler";
-import {CustomError} from "./types";
+import { CustomError } from "./types";
 
 export default async function getHistory(req: Request, res: Response) {
-    try {
-        const token = req.headers['authorization']?.replace("Bearer ", "");
-        await Token.validateAsync(token);
-        let result = await connectionDB.query(`SELECT sessions.token, sessions."userId" FROM sessions WHERE sessions.token = $1`, [token]);
-        if(result?.rowCount === 0) throw new CustomError("Unauthorized");
-        const { userId } = result.rows[0]
-        result = await connectionDB.query(`SELECT * FROM transactions WHERE "userId" = $1`, [userId]);
-        const cleanResults = result.rows.map((transaction) => {
-                                                                transaction.date = dayjs(transaction.date).format("DD/MM");
-                                                                return transaction;
-                                            });
-        res.status(200).send(cleanResults);
-    } catch (e) {
-        errorHandler(e,res);
-    }
+  try {
+    const token = req.headers["authorization"]?.replace("Bearer ", "");
+    await Token.validateAsync(token);
 
+    let result = await connectionDB.query(
+      `SELECT sessions.token, sessions."userId" FROM sessions WHERE sessions.token = $1`,
+      [token]
+    );
+
+    if (result?.rowCount === 0) throw new CustomError("Unauthorized");
+    const { userId } = result.rows[0];
+
+    result = await connectionDB.query(
+      `SELECT * FROM transactions WHERE "userId" = $1`,
+      [userId]
+    );
+    let depositsTotal = 0;
+    let withdrawalTotal = 0;       
+    const cleanResults = result.rows.map((transaction) => {
+      transaction.date = dayjs(transaction.date).format("DD/MM");
+      if(transaction.type === "deposit") {
+          depositsTotal += transaction.value;
+      } else {
+          withdrawalTotal += transaction.value
+      }
+      return transaction;
+    });
+    res.status(200).send({transactions: cleanResults, total: depositsTotal - withdrawalTotal});
+  } catch (e) {
+    errorHandler(e, res);
+  }
 }
